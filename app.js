998b25e6575b205cc2c2171b2a3eab8f5f1d66ea
margaret-jihua <margaret.jihua@gmail.com>
2020-07-15 14:23:00 -0700

    [loops] Add while loops

--- app.js
+++ app.js
@@ -9,3 +9,18 @@ console.log(num);
 
 var fruits = ["apple", "pear", "orange", "banana"];
 console.log(fruits);
+
+//while loop
+var number = 10;
+var init = 1;
+while(init < number){
+    console.log(init);
+    init += 1;
+}
+
+var bigNum = 20;
+var smallNum = 10;
+while (smallNum < bigNum){
+    console.log(smallNum);
+    smallNum += 1;
+}
