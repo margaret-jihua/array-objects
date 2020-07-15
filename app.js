77d4d4e24e1a1ab9794dc63f85697cb36a932eb2
margaret-jihua <margaret.jihua@gmail.com>
2020-07-15 14:03:48 -0700

    [array] Add fruits

---
+++ app.js
@@ -0,0 +1,11 @@
+var name = "Mars";
+console.log(name);
+
+var city = "San Francisco";
+console.log(city);
+
+var num = 7;
+console.log(num);
+
+var fruits = ["apple", "pear", "orange", "banana"];
+console.log(fruits);
